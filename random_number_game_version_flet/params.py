d01540514a83d8a501b730f13caa898076a1329c
ivanarganda <ivanartista96@gmail.com>
2025-10-17 15:57:12 +0200

    first deploy

--- random_number_game_version_flet/params.py
+++
@@ -1,9 +0,0 @@
-# Detect the protocol to use
-import os
-
-
-REQUEST_URL_TEST = "http://127.0.0.1:5000"
-REQUEST_URL_PRODUCTION = "https://api.example.com"
-
-HEADERS = { 'Content-Type': 'application/json'}
-
